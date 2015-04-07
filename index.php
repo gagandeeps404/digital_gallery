9b4f31d2e762cb3e3e93ce80c37cc83da8278884
Gagandeep Singh Ranu <gr98094n@pace.edu>
2015-04-07 00:54:54 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php include($_SERVER['DOCUMENT_ROOT'].'/header.php'); ?>
+
+  <div class="banner"><img src="img/book.jpg"></div>
+  <article class="container">
+    <h2>EVENTS <span class="small-title">SEE ALL</span></h2>
+    </p>
+
+   <div id="owl-demo" class="owl-carousel">
+          
+                <div class="item"><img src="images/2009/2009-howe-1.jpg" alt="Owl Image"></div>
+                <div class="item"><img src="images/2009/2009-margruder-1.jpg" alt="Owl Image"></div>
+                <div class="item"><img src="images/2009/2009-mckay-1.jpg" alt="Owl Image"></div>
+                <div class="item"><img src="images/2009/2009-wattenberg-1.jpg" alt="Owl Image"></div>
+                <div class="item"><img src="images/2009/2009-rothenberg-1.jpg" alt="Owl Image"></div>
+                <div class="item"><img src="images/2009/2009-matasavich.jpg" alt="Owl Image"></div>
+                <div class="item"><img src="images/2009/2009-fontaine-1.jpg" alt="Owl Image"></div>
+                <div class="item"><img src="images/2009/2009-grames-1.jpg" alt="Owl Image"></div>
+ 
+</div>
+
+    </p>
+    <br/>
+    <br/>
+     <h2>YEAR <span class="small-title">SEE ALL</span></h2>
+    </p>
+    <div id="owl-demo2" class="owl-carousel">
+          
+                <div class="item"><img src="images/artists/2005-image.jpg" alt="Owl Image"></div>
+                <div class="item"><img src="images/artists/2006-image.jpg" alt="Owl Image"></div>
+                <div class="item"><img src="images/artists/2007-image.jpg" alt="Owl Image"></div>
+                <div class="item"><img src="images/artists/2008-image.jpg" alt="Owl Image"></div>
+                <div class="item"><img src="images/artists/2009-image.jpg" alt="Owl Image"></div>
+                <div class="item"><img src="images/artists/2010-image.jpg" alt="Owl Image"></div>
+                <div class="item"><img src="images/artists/2011-image.jpg" alt="Owl Image"></div>
+                <div class="item"><img src="images/artists/2012-image.jpg" alt="Owl Image"></div>
+				<div class="item"><img src="images/artists/2013-image.jpg" alt="Owl Image"></div>
+ 
+</div>
+  </article>
+<?php include($_SERVER['DOCUMENT_ROOT'].'/footer.php'); ?>
